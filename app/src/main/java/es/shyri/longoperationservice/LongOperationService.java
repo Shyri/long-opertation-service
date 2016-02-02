6b5a03d99ed6d0a7e6ff6098ca5c2116cfbf1b7a
Shyri Villar <igs.villar@gmail.com>
2016-02-02 08:56:59 +0100

    Add basic starting and running states to service

--- app/src/main/java/es/shyri/longoperationservice/LongOperationService.java
+++ app/src/main/java/es/shyri/longoperationservice/LongOperationService.java
@@ -37,6 +37,90 @@ public class LongOperationService extends Service {
     public IBinder onBind(Intent intent) {
         return mBinder;
     }
+
+    public void performLongOperation() {
+        currentStatus = STATUS.STARTING;
+        updateStatus();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int i = 0;
+                int percentage = 0;
+
+                //////////////////////// Remain in Starting status for a while
+                while(percentage < 100) {
+                    i++;
+                    if(i == 20000000) {
+                        percentage++;
+                        i = 0;
+                    }
+                }
+                ////////////////////////
+
+                i = 0;
+                percentage = 0;
+
+                currentStatus = STATUS.RUNNING;
+                updateStatus();
+
+                //////////////////////// Start long operation
+                while(percentage < 100) {
+                    i++;
+                    if(i == 30000000) {
+                        percentage++;
+                        updateProgress(percentage);
+                        i = 0;
+                    }
+                }
+                ////////////////////////
+
+
+            }
+        }).start();
+    }
+
+    private void updateStatus() {
+        switch(currentStatus) {
+            case STARTING: {
+                NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
+                notificationBuilder.setContentTitle(getString(R.string.notification_title))
+                        .setContentText(getString(R.string.notification_message_starting))
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setOngoing(true);
+                startForeground(NOTIFICATION_ID, notificationBuilder.build());
+                break;
+            }
+            case RUNNING: {
+                NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
+                notificationBuilder.setContentTitle(getString(R.string.notification_title))
+                        .setContentText(getString(R.string.notification_message_running))
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setOngoing(true)
+                        .setContentInfo("0%");
+                startForeground(NOTIFICATION_ID, notificationBuilder.build());
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
+    private void updateProgress(int progress) {
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
+        notificationBuilder.setContentTitle(getString(R.string.notification_title))
+                .setContentText(getString(R.string.notification_message_running))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setOngoing(true)
+                .setProgress(100, Integer.valueOf(progress), false)
+                .setContentInfo(progress + "%");
+        startForeground(NOTIFICATION_ID, notificationBuilder.build());
+    }
+
+    public void cancelLongOperation() {
+
+    }
+
     public STATUS getCurrentStatus() {
         return currentStatus;
     }
