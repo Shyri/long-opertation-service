69dc6eca79f63db5d685d2a70f6440ff790b1222
Shyri Villar <igs.villar@gmail.com>
2016-02-02 08:56:02 +0100

    Add notification manager to service

--- app/src/main/java/es/shyri/longoperationservice/LongOperationService.java
+++ app/src/main/java/es/shyri/longoperationservice/LongOperationService.java
@@ -1,17 +1,27 @@
 package es.shyri.longoperationservice;
 
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v7.app.NotificationCompat;
 
 /**
  * Created by Shyri on 01/02/2016.
  */
 public class LongOperationService extends Service {
+    public static final int NOTIFICATION_ID = 1234;
+    private NotificationManager nm;
 
     private final IBinder mBinder = new LocalBinder();
+
+    public void onCreate() {
+        super.onCreate();
+        nm = (NotificationManager)  getSystemService(NOTIFICATION_SERVICE);
+
+    }
 
     @Nullable
     @Override
